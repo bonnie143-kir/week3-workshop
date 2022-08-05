6c9e62c3aa89f1045212ff5d16eb4d0772bfa0e4
Bongraoi Arebaio <bongraoi.arebaio@griffithuni.edu.au>
2022-08-05 11:13:09 +1000

    added script.js

---
+++ www/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $("#loginform").submit(function(event){
+        event.preventDefault();
+        ajaxPost();
+    });
+
+    function ajaxPost(){
+        var formData ={
+            email: $("#email").val(), upwd: $("#upwd").val()
+        }
+
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            url: window.location + "api/login",
+            data: JSON.stringify(formData),
+            dataType: 'json',
+            success : function(user) {
+                if (user.valid) == true) {
+                    $("#loginform").addClass("success");
+                    $("#loginform").removeClass("fail");
+                }else {
+                    $("#loginform").removeClass("success");
+                    $("#loginform").addClass("fail");
+                }
+            $("#postResultDiv").html("<p>" + "Post sucess! <br>"+ "Email addres: " + user.email + "Password: " + user.upwd + "</br>" +"Valid user: " + user.valid + "</p>");
+            },
+            error: function(e){
+                alert("Error!");
+                console.log("ERROR: ", e);
+            }
+        });
+    resetData();
+    }
+    function resetData(){
+        $("#email").val("");
+        $("#upwd").val("");
+    } 
+});

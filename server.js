4c412c9e79fc78ac8dac192248a6128fdd1e4201
Bongraoi Arebaio <bongraoi.arebaio@griffithuni.edu.au>
2022-08-05 10:55:20 +1000

    TASK 3, creating server routes, server.js and html files corresponding to routes

--- server.js
+++ server.js
@@ -12,3 +12,29 @@ app.listen(3000, ()=>{
     var m = d.getMinutes();
     console.log('Server has started listening at: ' + n + ":" +m); 
 });
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/www/homepage.html');
+});
+
+app.get('/accountDetails', function(req, res){
+    res.sendFile(__dirname + 'www/accountDetails.html');
+});
+
+app.post('/api/login', function(req, res){
+    let users = [{'email': 'bongii@outmail.com', 'pwd': 'bongii'}, {'email': 'abcd@efg.com':'pwd':'hijk'}, {'email':'12345@sixseven.com', 'pwd': 1234}];
+
+    if (!req.body){
+        return res.sendStatus(400)
+    }
+        var user = {};
+        user.email = req.body.email;
+        user.upwd = req.body.upwd;
+        user.valid = false;
+    for (let i=0;i<users.length;i++){
+        if (req.body.email == users[i].email && req.body.upwd == users[i].pwd){
+            user.valid = true;
+        }
+    }   
+        res.send(user);
+});
